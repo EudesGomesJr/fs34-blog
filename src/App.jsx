f10975d5bfb3da89ef295df0d380246bcdd6300a
Eudes Gomes <eudes_jr@hotmail.com>
2024-06-22 11:47:40 -0300

    creiando menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import { useState } from 'react'
+
+function Header({children}  ) {
+  return (
+    <header className=' bg-white'>
+      <nav className='mx-auto flex items-center p-6 shadow-md'>
+        {children}
+      </nav>
+    </header>
+  )
+}
+
+function HeaderLink({href, children}) {
+  return (
+    <a href={href} className='font-semibold leading-6 text-black ml-5'>{children}</a>
+  )
+}
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <>
+      <Header>
+        <HeaderLink href="https://company">
+            Company
+          </HeaderLink>
+          <HeaderLink href="https://marketplace"> 
+            Marketplace
+          </HeaderLink>
+          <HeaderLink> Teste </HeaderLink>
+      </Header>
+    </>
+
   )
 }
 
